83f4399ae6c7cfa40e3882147015453b00312686
WebDevAlok <bhushan.alok13@gmail.com>
2023-10-31 20:21:33 +0530

    Partial implementation for touch devices

--- myscript.js
+++ myscript.js
@@ -9,12 +9,24 @@ draggables.forEach( draggable => {
     draggable.addEventListener('dragend', () => {
         draggable.classList.remove('dragging');
     })
+
+    draggable.addEventListener('touchstart', () => {
+        draggable.classList.add('dragging');
+    })
+
+    draggable.addEventListener('touchend', () => {
+        draggable.classList.remove('dragging');
+    })
+
+    draggable.addEventListener('touchcancel', () => {
+        draggable.classList.remove('dragging');
+    })
 })
 
 containers.forEach( container => {
     container.addEventListener('dragover', e => {
         e.preventDefault();
-        const afterElement = getDragAfterElement(container, e.clientY);
+        const afterElement = getDragAfterElement(container,e.clientY);
         const draggable = document.querySelector('.dragging');
         if (afterElement) {
             container.insertBefore(draggable,afterElement);
@@ -22,10 +34,25 @@ containers.forEach( container => {
             container.appendChild(draggable);
         }
     })
+
+    container.addEventListener('touchmove', e => {
+        [...e.changedTouches].forEach(touch => {
+            const afterElement = getDragAfterElement(container,touch.clientY);
+            console.log(afterElement);
+            const draggable = document.querySelector('.dragging');
+            console.log(touch);
+            if (afterElement) {
+                const containerNew = afterElement.parentElement;
+                containerNew.insertBefore(draggable,afterElement);
+            } else {
+                container.appendChild(draggable);
+            }
+        })
+    })
 })
 
-function getDragAfterElement(container,y) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+function getDragAfterElement(container, y) {
+    const draggableElements = [...document.querySelectorAll('.draggable:not(.dragging)')];
 
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect();
